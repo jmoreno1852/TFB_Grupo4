2f4937df7514de60d52a184c4bfd41f2afaac082
jmoreno1852 <javiermmvv02@gmail.com>
2025-12-26 15:03:59 +0100

    feat(database): add async MongoDB connection management

---
+++ app/database.py
@@ -0,0 +1,31 @@
+#Import async client for MongoDB
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+#Import settings from config.py
+from app.config import settings
+
+#Global variables
+_client: AsyncIOMotorClient | None = None
+_db: AsyncIOMotorDatabase | None = None
+
+#Function to get MongoDB Client
+def get_client() -> AsyncIOMotorClient:
+    global _client
+    if _client is None:
+        _client = AsyncIOMotorClient(settings.MONGO_URI)
+    return _client
+
+#Function to get MongoDB Database
+def get_db() -> AsyncIOMotorDatabase:
+    global _db
+    if _db is None:
+        client = get_client()
+        _db = client[settings.MONGO_DB_NAME]
+    return _db
+
+#Function to close connection to MongoDB
+async def close_db():
+    global _client,_db
+    if _client is not None:
+        _client.close()
+    _client = None
+    _db = None
