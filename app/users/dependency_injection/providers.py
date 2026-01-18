c9c3eeb75b8560f0df4d974d3560f86a0c37b51d
jmoreno1852 <javiermmvv02@gmail.com>
2026-01-18 18:05:20 +0100

    feat(users): add DI providers for users module

---
+++ app/users/dependency_injection/providers.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+
+from motor.motor_asyncio import AsyncIOMotorDatabase
+
+from app.database import get_db
+from app.users.domain.services import UsersService
+from app.users.persistence.repository import MongoUserProfileRepository
+
+
+@lru_cache
+def build_user_profile_repository() -> MongoUserProfileRepository:
+    db: AsyncIOMotorDatabase = get_db()
+    repo = MongoUserProfileRepository(db)
+    return repo
+
+
+@lru_cache
+def build_users_service() -> UsersService:
+    repo = build_user_profile_repository()
+    return UsersService(repo)
+
+
+def clear_users_caches() -> None:
+    """
+    Clears cached DI singletons for users module, only for testing purposes
+    """
+    build_users_service.cache_clear()
+    build_user_profile_repository.cache_clear()
