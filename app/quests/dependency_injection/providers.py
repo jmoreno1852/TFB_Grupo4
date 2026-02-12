02cac0d96f36d7f00861f0e523b1a927da39910c
jmoreno1852 <javiermmvv02@gmail.com>
2026-02-12 22:35:33 +0100

    feat(quests): Add DI layer for quests module

---
+++ app/quests/dependency_injection/providers.py
@@ -0,0 +1,54 @@
+from functools import lru_cache
+
+from app.database import get_db
+
+from app.quests.domain.services import QuestsService
+from app.quests.persistence.mongo.repository import QuestCatalogMongoRepository, UserQuestMongoRepository
+
+
+
+@lru_cache(maxsize=1)
+def build_quest_catalog_repository() -> QuestCatalogMongoRepository:
+    """
+    Build and return QuestCatalogMongoRepository instance.
+    """
+    return QuestCatalogMongoRepository(get_db())
+
+
+@lru_cache(maxsize=1)
+def build_user_quest_repository() -> UserQuestMongoRepository:
+    """
+    Build and return UserQuestMongoRepository instance.
+    """
+    return UserQuestMongoRepository(get_db())
+
+
+def build_reward_applier(): #Need to wait to implement it in progression module
+    """
+    Build and return RewardApplier implementation.
+    """
+    raise NotImplementedError("RewardApplier provider not wired yet.")
+
+
+@lru_cache(maxsize=1)
+def build_quests_service() -> QuestsService:
+    """
+    Build and return QuestsService instance.
+    """
+    return QuestsService(
+        quest_catalog_repository=build_quest_catalog_repository(),
+        user_quest_repository=build_user_quest_repository(),
+        reward_applier=build_reward_applier(),
+        active_quests_per_guild=3,
+        recent_completed_limit=10,
+    )
+
+
+def clear_caches() -> None:
+    """
+    Clear lru_cache for DI providers.
+    Useful in testing when FastAPI TestClient recreates the app multiple times.
+    """
+    build_quests_service.cache_clear()
+    build_quest_catalog_repository.cache_clear()
+    build_user_quest_repository.cache_clear()
