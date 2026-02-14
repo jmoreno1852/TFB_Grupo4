8a9e659d11bd73639fab5d310b5ee95ce1592633
jmoreno1852 <javiermmvv02@gmail.com>
2026-02-14 23:26:20 +0100

    feat(main): Add router and initialization of indexes for progression module

--- app/main.py
+++ app/main.py
@@ -15,21 +15,25 @@ from app.guilds.api.router import router as guilds_router
 from app.guilds.dependency_injection.providers import build_guild_repository, build_membership_repository
 # Quest imports
 from app.quests.api.router import router as quests_router
-from app.quests.dependency_injection.providers import (
-    build_quest_catalog_repository,
-    build_user_quest_repository,
-)
+from app.quests.dependency_injection.providers import build_quest_catalog_repository, build_user_quest_repository
+#Progression imports
+from app.progression.api.router import router as progression_router
+from app.progression.dependency_injection.providers import build_progression_repository
+
 #Imports to clear cache for testing purposes with TestClient
 from app.auth.dependency_injection.providers import clear_caches as clear_auth_caches
 from app.users.dependency_injection.providers import clear_users_caches
 from app.guilds.dependency_injection.providers import clear_caches as clear_guilds_caches
 from app.quests.dependency_injection.providers import clear_caches as clear_quests_caches
+from progression.dependency_injection.providers import clear_caches as clear_progression_caches
+
 
 CACHE_CLEAR_FUNCTIONS = [
     clear_auth_caches,
     clear_users_caches,
     clear_guilds_caches,
     clear_quests_caches,
+    clear_progression_caches,
 ]
 
 #Startup event to ensure indexes uniqueness in the database
@@ -50,6 +54,9 @@ async def lifespan(app: FastAPI):
     repo_quest_catalog = build_quest_catalog_repository()
     repo_user_quest = build_user_quest_repository()
 
+    #Progression Repository
+    repo_progression = build_progression_repository()   
+
     #startup ensure indexes uniqueness of email values
     await repo_user.ensure_indexes()
     await repo_profile.ensure_indexes()
@@ -61,6 +68,9 @@ async def lifespan(app: FastAPI):
     # startup ensure indexes for quests
     await repo_quest_catalog.ensure_indexes()
     await repo_user_quest.ensure_indexes()
+
+    # startup ensure indexes for progression
+    await repo_progression.ensure_indexes()
 
     #yield this point to startup completion
     yield
@@ -82,7 +92,8 @@ def create_app() -> FastAPI:
     app.include_router(users_router)
     app.include_router(guilds_router)
     app.include_router(quests_router)
-    
+    app.include_router(progression_router)
+
     #Health check endpoint for testing purposes
     @app.get("/health")
     async def health():
