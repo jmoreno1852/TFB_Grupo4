9bcbd8b9ae239e7fb2da0567687ebc7a5883bc5d
jmoreno1852 <javiermmvv02@gmail.com>
2026-03-01 02:52:36 +0100

    fix(users): Fix bug in UserProfileRepository.create() that caused first request of GET /users/me to return NoneType error

--- app/users/persistence/mongo/repository.py
+++ app/users/persistence/mongo/repository.py
@@ -22,6 +22,8 @@ class MongoUserProfileRepository(UserProfileRepository):
     async def create(self, profile: UserProfile) -> UserProfile:
         try: 
             res = await self._collection.insert_one(profile_to_doc(profile))
+            doc = await self._collection.find_one({"_id": res.inserted_id})
+            return doc_to_profile(doc) if doc else None
         except DuplicateKeyError:
             #If exception profile already exists so we can return doc from mongo
             exists = await self._collection.find_one({"user_id": profile.user_id})
